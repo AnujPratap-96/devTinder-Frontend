844410e6255ca78ab0074df58b9d3b8bc125ae69
AnujPratap-96 <offcialthakur94@gmail.com>
2026-07-19 01:58:52 +0530

    changed the backend url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "/api";
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "https://dev-tinder-eight-kappa.vercel.app/";
 
 import io from "socket.io-client";
 
